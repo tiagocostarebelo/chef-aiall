6864a77c7a88cb3f7d959660c9dd1ad892c6edb2
Tiago Costa Rebelo <43050965+tiagocostarebelo@users.noreply.github.com>
2025-06-24 14:55:10 +0000

    added ingredient list section and check for recipe button and updated styles

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -31,8 +31,17 @@ export default function Main() {
                 />
                 <button className="add-ingredient__button" >Add ingredient</button>
             </form>
-
-            {newListIngredients}
+            <section>
+                <h2>Ingredients on hand:</h2>
+                <ul className="ingredients-list" aria-live="polite">{newListIngredients}</ul>
+                <div className="get-recipe-container">
+                    <div>
+                        <h3>Ready for a recipe?</h3>
+                        <p>Generate a recipe from your list of ingredients.</p>
+                    </div>
+                    <button>Get a recipe</button>
+                </div>
+            </section>
         </main>
     )
 }
