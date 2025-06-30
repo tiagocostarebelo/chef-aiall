51523479420d1a06429f65d84b8fab371810396a
Tiago Costa Rebelo <costarebelo.tiago@gmail.com>
2025-06-30 17:13:21 +0100

    updated Recipe component to render markdown using react-markdown npm package.

--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -1,7 +1,10 @@
+import ReactMarkdown from 'react-markdown'
+
 export default function Recipe(props) {
     return (
-        <section>
-            {props.recipe}
+        <section className="suggested-recipe-container" aria-live="polite">
+            <h2>Chef AiAll Recommends:</h2>
+            <ReactMarkdown>{props.recipe}</ReactMarkdown>
         </section>
     )
 }
